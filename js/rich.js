639fa3ac4deb830b343ddcb2bf6de415788a5fb9
Kyryll <sotnikov_k@outlook.com>
2018-08-02 16:41:58 +0300

    rich

---
+++ js/rich.js
@@ -0,0 +1,49 @@
+$('.slider-juice').slick({
+    dots: false,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.slider-icons',
+});
+$('.slider-icons').slick({
+    dots: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    verticalSwiping: true,
+    vertical: true,
+    focusOnSelect: true,
+    asNavFor: '.slider-juice',
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 4
+            }
+        },
+        {
+            breakpoint: 766,
+            settings: {
+                slidesToShow: 3,
+                verticalSwiping: false,
+                vertical: false,
+            }
+        },
+        {
+            breakpoint: 400,
+            settings: {
+                slidesToShow: 2,
+                verticalSwiping: false,
+                vertical: false,
+            }
+        }
+    ],
+    prevArrow: '<div class="arrow left"><img src="img/icons/left-arrow.svg"></div>',
+    nextArrow: '<div class="arrow right"><img src="img/icons/right-arrow.svg"></div>',
+});
+function slowScroll (id) {
+    var offset = 0;
+    $('html, body').animate ({
+        scrollTop: $(id).offset ().top - offset
+    }, 800);
+    return false;
+};
